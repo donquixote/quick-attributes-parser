4762375c0d84ad78e11b4c6256bb5508afae73c2
Andreas Hennings <andreas@dqxtech.net>
2021-11-15 16:08:45 +0100

    Remove unused imports.

--- src/FileTokens/FileTokens_Common.php
+++ src/FileTokens/FileTokens_Common.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Donquixote\QuickAttributes\FileTokens;
 
-use Donquixote\QuickAttributes\Exception\ParserException;
 use Donquixote\QuickAttributes\Util\TokenizerUtil;
 
 class FileTokens_Common implements FileTokensInterface {
--- tests/src/ParserUtilTest.php
+++ tests/src/ParserUtilTest.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace Donquixote\QuickAttributes\Tests;
 
-use Donquixote\QuickAttributes\AttributeCommentParser\AttributeCommentParser;
-use Donquixote\QuickAttributes\Exception\ParserException;
-use Donquixote\QuickAttributes\FileTokens\FileTokens_Common;
-use Donquixote\QuickAttributes\Parser\FileParser;
-use Donquixote\QuickAttributes\Tests\Util\TestArrayUtil;
-use Donquixote\QuickAttributes\Tests\Util\TestExportUtil;
 use Donquixote\QuickAttributes\Util\ParserUtil;
 use PHPUnit\Framework\TestCase;
 
