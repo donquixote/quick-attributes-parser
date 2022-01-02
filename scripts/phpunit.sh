393848f3fbb164d54bf0728c4616a8539b7086bd
Andreas Hennings <andreas@dqxtech.net>
2022-01-02 02:59:36 +0100

    Script to run phpunit in different php versions.

---
+++ scripts/phpunit.sh
@@ -0,0 +1,3 @@
+/usr/bin/php7.4 -d zend.assertions=1 -d assert.exception=1 ./vendor/bin/phpunit
+/usr/bin/php8.0 -d zend.assertions=1 -d assert.exception=1 ./vendor/bin/phpunit
+/usr/bin/php8.1 -d zend.assertions=1 -d assert.exception=1 ./vendor/bin/phpunit
