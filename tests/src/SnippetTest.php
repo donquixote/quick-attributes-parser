4ee5ea14ef0e4d79ecd64a4731b879c953543fcf
Andreas Hennings <andreas@dqxtech.net>
2022-01-09 06:49:09 +0100

    Fix: Allow FQN and QN for PHP 8 in FileParser::skipReturnType().

--- tests/src/SnippetTest.php
+++ tests/src/SnippetTest.php
@@ -18,12 +18,7 @@ use Donquixote\QuickAttributes\Tests\Util\TestExportUtil;
  *   php: string,
  *   php_version_ids?: list<int>,
  *   importss: array<string, array<string, string>>,
- *   'importss.php8'?: array<string, array<string, string>>,
  *   attributess: array<string, array{
- *     imports?: array<string, string>,
- *     'attr-comments'?: list<string>,
- *   }>,
- *   'attributess.php8'?: array<string, array{
  *     imports?: array<string, string>,
  *     'attr-comments'?: list<string>,
  *   }>,
@@ -75,9 +70,7 @@ class SnippetTest extends YmlTestBase {
       'php',
       'php_version_ids',
       'importss',
-      'importss.php8',
       'attributess',
-      'attributess.php8',
       'tokenizer_split',
       'exception',
       'exception.php8',
