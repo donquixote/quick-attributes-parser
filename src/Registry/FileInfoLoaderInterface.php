2822494319a0e33d67eeac24cae9652063bcddab
Andreas Hennings <andreas@dqxtech.net>
2022-01-09 19:29:39 +0100

    Refac: Extract FileInfoLoaderInterface.

---
+++ src/Registry/FileInfoLoaderInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ */
+
+declare(strict_types=1);
+
+namespace Donquixote\QuickAttributes\Registry;
+
+use Donquixote\QuickAttributes\SymbolInfo\File\FileInfoInterface;
+
+interface FileInfoLoaderInterface {
+
+  /**
+   * @param string $file
+   *
+   * @return \Donquixote\QuickAttributes\SymbolInfo\File\FileInfoInterface
+   *
+   * @throws \Donquixote\QuickAttributes\Exception\ParserException
+   */
+  public function loadFile(string $file): FileInfoInterface;
+
+  /**
+   * @param string $file
+   *
+   * @return FileInfoInterface|null
+   *
+   * @throws \Donquixote\QuickAttributes\Exception\ParserException
+   */
+  public function loadUnknownFile(string $file): ?FileInfoInterface;
+
+}
