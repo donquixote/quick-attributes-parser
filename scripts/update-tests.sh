45c340ad1fd8f98ce98d399e8e6397d7fb9323f0
Andreas Hennings <andreas@dqxtech.net>
2022-01-09 06:09:02 +0100

    Testing: Add script to update all tests at once.

---
+++ scripts/update-tests.sh
@@ -0,0 +1,3 @@
+UPDATE_TESTS=1 /usr/bin/php7.4 -d zend.assertions=1 -d assert.exception=1 ./vendor/bin/phpunit
+UPDATE_TESTS=1 /usr/bin/php8.0 -d zend.assertions=1 -d assert.exception=1 ./vendor/bin/phpunit
+UPDATE_TESTS=1 /usr/bin/php8.1 -d zend.assertions=1 -d assert.exception=1 ./vendor/bin/phpunit
