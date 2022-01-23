76af4d7137609962fd5b10821ce598314962edbe
Andreas Hennings <andreas@dqxtech.net>
2022-01-23 15:09:10 +0100

    CS: Docs

--- src/SymbolVisitor/FunctionLike/ParamVisitor_NoOp.php
+++ src/SymbolVisitor/FunctionLike/ParamVisitor_NoOp.php
@@ -6,10 +6,16 @@ namespace Donquixote\QuickAttributes\SymbolVisitor\FunctionLike;
 
 class ParamVisitor_NoOp implements ParamVisitorInterface {
 
+  /**
+   * {@inheritdoc}
+   */
   public function addParameter(string $name, array $attributes): void {
     // Do nothing.
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function markAsComplete(): void {
     // Do nothing.
   }
